9d19317b560a5d197daa68d85a5edb1ffed3cef2
Geovanny Neves Liberato <gnliberato@outlook.com>
2025-07-14 14:46:53 -0300

    Criado validadores para equipamentos eletrônicos

---
+++ backend/desafioInventBackend/desafioInventBackend/Model/Validator/EquipamentoEletronicoValidator.cs
@@ -0,0 +1,18 @@
+﻿using DesafioInventBackend.Model.Entity;
+using FluentValidation;
+
+namespace DesafioInventBackend.Model.Validator
+{
+    public class EquipamentoEletronicoValidator: AbstractValidator<EquipamentoEletronico>
+    {
+
+        public EquipamentoEletronicoValidator() {
+
+            RuleFor(ee => ee.Nome).NotNull().NotEmpty();
+            RuleFor(ee => ee.TipoEquipamento).NotNull();
+            RuleFor(ee => ee.QuantidadeEstoque).NotNull();
+            RuleFor(ee => ee.TemEstoque).NotNull();
+            RuleFor(ee => ee.DataInclusao).NotNull();
+        }
+    }
+}
