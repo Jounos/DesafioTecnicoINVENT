71ef34b1507b7f8fa4e9c030b6974c208dc13eb9
Geovanny Neves Liberato <gnliberato@outlook.com>
2025-07-16 17:53:31 -0300

    Iniciado desenvolvimento dos testes unitários

---
+++ backend/desafioInventBackend/DesafioInventTest/InMemoryRepositoryText.cs
@@ -0,0 +1,18 @@
+﻿using DesafioInventBackend.Model.Entity;
+using DesafioInventBackend.Repository;
+using DesafioInventBackend.Service;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioInventTest
+{
+    internal class InMemoryRepositoryText
+    {
+
+    }
+}
