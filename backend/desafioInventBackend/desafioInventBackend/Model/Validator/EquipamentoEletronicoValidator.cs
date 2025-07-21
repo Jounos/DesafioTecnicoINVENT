38b73c2193c6f758e7aac96d6225510318ef6f0a
Geovanny Neves Liberato <gnliberato@outlook.com>
2025-07-21 18:05:12 -0300

    Refatorado validador de data de inclusão

--- backend/desafioInventBackend/desafioInventBackend/Model/Validator/EquipamentoEletronicoValidator.cs
+++ backend/desafioInventBackend/desafioInventBackend/Model/Validator/EquipamentoEletronicoValidator.cs
@@ -11,28 +11,18 @@ namespace DesafioInventBackend.Model.Validator
             RuleFor(ee => ee.Nome).NotNull().NotEmpty().MinimumLength(3).MaximumLength(100);
             RuleFor(ee => (int)ee.TipoEquipamento).NotNull().InclusiveBetween(1, 4);
             RuleFor(ee => ee.QuantidadeEstoque).NotNull().GreaterThanOrEqualTo(0);
-            RuleFor(ee => ee.DataInclusao).NotNull().Must(DeveSerDataRecente);
+            RuleFor(ee => ee.DataInclusao).NotNull().Must(_deveSerDataRecente);
             RuleFor(ee => ee.DataExclusao).Null();
         }
 
-        private bool DeveSerDataRecente(DateTimeOffset date)
+        private bool _deveSerDataRecente(DateTimeOffset date)
         {
-            if (date.Equals(DateTimeOffset.MinValue))
+            if (date.Equals(DateTimeOffset.MinValue) || date.Equals(DateTimeOffset.MaxValue))
             {
                 return false;
             }
 
-            if (date.Equals(DateTimeOffset.MaxValue))
-            {
-                return false;
-            }
-
-            if (DateTimeOffset.Compare(date, new DateTime(2009, 12, 31)) < 0)
-            {
-                return false;
-            }
-
-            if (DateTimeOffset.Compare(date, new DateTime(2030, 1, 1)) > 0)
+            if ((DateTimeOffset.Compare(date, new DateTime(2009, 12, 31)) < 0) && (DateTimeOffset.Compare(date, new DateTime(2030, 1, 1)) > 0))
             {
                 return false;
             }
