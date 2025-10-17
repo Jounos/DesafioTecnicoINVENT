3d6956914ff0a9d99d2527ac3cf7b6b759004e9a
Geovanny Neves Liberato <geovanny.liberato@inventsoftware.com.br>
2025-10-17 18:10:11 -0300

    Corrigido padrozinação do projeto

--- backend/desafioInventBackend/desafioInventBackend/wwwroot/src/Component.js
+++ backend/desafioInventBackend/desafioInventBackend/wwwroot/src/Component.js
@@ -1,27 +1,55 @@
 sap.ui.define([
-    "sap/ui/core/UIComponent",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel",
-    "sap/ui/Device"
-], (UIComponent, JSONModel, ResourceModel, Device) => {
-    "use strict";
+	"sap/ui/core/UIComponent",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/Device",
+	"sap/ui/model/resource/ResourceModel",
+], (UIComponent, JSONModel, Device, ResourceModel) => {
+	"use strict";
 
-    return UIComponent.extend("invent.desafio", {
-        metadata: {
-            manifest: "json"
-        },
+	return UIComponent.extend("desafio.Component", {
+		metadata: {
+			manifest: "json"
+		},
 
-        init() {
-            UIComponent.prototype.init.apply(this, arguments);
+		init() {
+
+			const nomeModelo = "device";
+			this.setModel(this._createDeviceModel(), nomeModelo);
 
 			const i18nModel = new ResourceModel({
-                bundleName: "invent.desafio.i18n.i18n"
+                bundleName: "desafio.i18n.i18n"
             });
             this.setModel(i18nModel, "i18n");
-        },
 
-        getContentDensityClass() {
-			return Device.support.touch ? "sapUiSizeCozy" : "sapUiSizeCompact";
-		}
-    });
+			UIComponent.prototype.init.apply(this, arguments);
+		},
+
+		getContentDensityClass() {
+			if (this._sContentDensityClass === undefined) {
+				if (
+					jQuery(document.body).hasClass("sapUiSizeCozy") ||
+					jQuery(document.body).hasClass("sapUiSizeCompact")
+				) {
+					this._sContentDensityClass = "";
+				} else if (!Device.support.touch) {
+					this._sContentDensityClass = "sapUiSizeCompact";
+				} else {
+					this._sContentDensityClass = "sapUiSizeCozy";
+				}
+			}
+			return this._sContentDensityClass;
+		},
+
+		_createDeviceModel: function () {
+			var oModel = new JSONModel(Device);
+			oModel.setDefaultBindingMode("OneWay");
+			return oModel;
+		},
+
+
+		destroy: function () {
+			// call the base component's destroy function
+			UIComponent.prototype.destroy.apply(this, arguments);
+		},
+	});
 });
--- backend/desafioInventBackend/desafioInventBackend/wwwroot/src/app/listagem/Listagem.controller.js
+++ backend/desafioInventBackend/desafioInventBackend/wwwroot/src/app/listagem/Listagem.controller.js
@@ -6,7 +6,7 @@ sap.ui.define([
 ], (Controller) => {
     "use strict";
 
-    return Controller.extend("invent.desafio.listagem.Listagem", {
+    return Controller.extend("desafio.listagem.Listagem", {
 
     });
 });
