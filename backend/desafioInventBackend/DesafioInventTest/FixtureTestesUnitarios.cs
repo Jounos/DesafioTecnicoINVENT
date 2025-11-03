0fd419bea392c1770073a1ed96e25a158317ee95
Geovanny Neves Liberato <geovanny.liberato@inventsoftware.com.br>
2025-11-03 15:30:56 -0300

    Criado estrutura para utilizar o banco de dados raven em testes unitários

---
+++ backend/desafioInventBackend/DesafioInventTest/FixtureTestesUnitarios.cs
@@ -0,0 +1,43 @@
+﻿using Raven.Client.Documents;
+using Raven.Client.Documents.Session;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+
+namespace DesafioInventTest
+{
+    public class FixtureTestesUnitarios : IDisposable
+    {
+
+        public RavenDBTest RavenDBTest;
+
+        public FixtureTestesUnitarios()
+        {
+            RavenDBTest = new RavenDBTest();
+        }
+
+        public void AdicionarListaComId<T>(IDocumentSession session, List<KeyValuePair<string, T>> lista) where T : new()
+        {
+            RavenDBTest.AdicionarListaComId(session, lista);
+        }
+
+        public void SalvarAlteracoes(IDocumentSession session)
+        {
+            RavenDBTest.SalvarAlteracoes(session);
+        }
+
+        public void CancelarAlteracoes(IDocumentSession session)
+        {
+            RavenDBTest.CancelarAlteracoes(session);
+        }
+
+        public IDocumentStore ObterNovoStore(IDocumentSession session)
+        {
+            return RavenDBTest.ObterNovoStore(session);
+        }
+
+        public void Dispose()
+        {
+            RavenDBTest.Dispose();
+        }
+    }
+}
---
+++ backend/desafioInventBackend/DesafioInventTest/RavenDBTest.cs
@@ -0,0 +1,66 @@
+﻿using Raven.Client.Documents;
+using Raven.Client.Documents.Session;
+using Raven.TestDriver;
+
+namespace DesafioInventTest
+{
+    public class RavenDBTest : RavenTestDriver
+    {
+
+        public Dictionary<string, IDocumentStore> storesDosBancos { get; private set; }  
+        
+        public RavenDBTest()
+        {
+            storesDosBancos = new Dictionary<string, IDocumentStore>();
+        }
+
+        protected override void PreInitialize(IDocumentStore documentStore)
+        {
+            documentStore.Conventions.MaxNumberOfRequestsPerSession = 10;
+            documentStore.Conventions.UseOptimisticConcurrency = false;
+            documentStore.Conventions.IdentityPartsSeparator = '-';
+            documentStore.Conventions.SaveEnumsAsIntegers = true;
+            base.PreInitialize(documentStore);
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            _destruirBancoDeDadosTest();
+        }
+
+        private void _destruirBancoDeDadosTest()
+        {
+            foreach (var storeNomeado in this.storesDosBancos)
+            {
+                storeNomeado.Value.Dispose();
+            }
+        }
+
+        public void SalvarAlteracoes(IDocumentSession session)
+        {
+            session.SaveChanges();
+            WaitForIndexing(session.Advanced.DocumentStore);
+            System.Threading.Thread.Sleep(100);
+        }
+
+        public void CancelarAlteracoes(IDocumentSession session)
+        {
+            session.Advanced.Clear();
+        }
+
+        public IDocumentSession ObterNovoStore(string nomeDoBanco)
+        {
+            var store = GetDocumentStore(database: nomeDoBanco);
+            storesDosBancos.Add(nomeDoBanco, store);
+
+            return (IDocumentSession)store; 
+        }
+
+        public void AdicionarListaComId<T>(IDocumentSession session, List<KeyValuePair<string, T>> lista) where T : new()
+        {
+            lista.ForEach(x => session.Store(x.Value, x.Key));
+            SalvarAlteracoes(session);
+        }
+    }
+}
