80edca5530113f737d84e0b4e7565119429d9690
Geovanny Neves Liberato <geovanny.liberato@inventsoftware.com.br>
2025-11-18 18:07:26 -0300

    Criado estrutura para testes opas da tela de edição

---
+++ backend/desafioInventBackend/desafioInventBackend/wwwroot/src/test/integration/atualizar/AtualizarEquipamentoEletronico.js
@@ -0,0 +1,19 @@
+sap.ui.define([
+	"sap/ui/test/Opa5"
+], function (Opa5) {
+	"use strict";
+
+	const VIEW_NAME = "salvar.Salvar";
+
+	Opa5.createPageObjects({
+		naPaginaEdicaoDeEquipamentosEletronicos: {
+			actions: {
+
+			},
+
+			assertions: {
+
+			}
+		}
+	});
+});
---
+++ backend/desafioInventBackend/desafioInventBackend/wwwroot/src/test/integration/atualizar/JornadaAtualizarEquipamentoEletronico.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+	"sap/ui/test/opaUnit",
+	"./AtualizarEquipamentoEletronico"
+], function (opaTest, AtualizarEquipamentoEletronico) {
+	"use strict";
+
+	QUnit.module("AtualizarEquipamentoEletronico");
+
+	opaTest("", function (Given, When, Then) {});
+});
